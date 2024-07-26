b7e49f9de6f251dbbaa3572e8ae9742b54a79a82
Judah92 <mattscallorn@gmail.com>
2024-07-26 18:28:20 -0500

    Initial C++ Commit
    Adding Elements to and Printing Elements from a Vector.

---
+++ SoloProjects/BookVectorProgram.cpp
@@ -0,0 +1,38 @@
+//C++ Refresher: if-else statements, Loops, and Arrays and Vectors. 
+#include <iostream>
+#include <vector>
+#include <string>
+//using std::cout
+//using std::vector;
+//using std::string;
+//using std::cout
+//using std::endl
+using namespace std;//contains all directives for vectors, strings, cin/cout, endl ect.
+
+
+int main()
+{
+    const int NUM_BOOKS_IN_STOCK = 6;
+    vector<string> allTheBooks(NUM_BOOKS_IN_STOCK);
+    unsigned int i;
+
+    cout << "Enter: " << NUM_BOOKS_IN_STOCK << " Book Names" << endl;
+    for (i = 0; i < allTheBooks.size(); ++i)
+    {
+        cout << "Book Name: ";
+        //cin >> allTheBooks.at(i);
+        getline(cin, allTheBooks.at(i));//getline accomodates whitespace in input for strings
+    }
+
+    cout << "You Entered: ";
+    for (i = 0; i < allTheBooks.size(); ++i)
+    {
+        cout << allTheBooks.at(i);
+        if (i < allTheBooks.size() - 1)//Check if it's not last element in vector
+        {
+            cout << ", ";
+        }
+    }
+
+    return 0;
+}
